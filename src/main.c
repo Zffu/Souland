307ca5c20063b2d6ffc66ed5b95caa84c6ac0f61
Zffu <radi0o0@outlook.com>
2024-10-18 01:49:30 +0200

    pushed code

--- src/main.c
+++ src/main.c
@@ -2,6 +2,46 @@
  * The main file of Soulsand.
  */
 
-int main() {
+#include <stdio.h>
+#include <string.h>
+
+FILE *filePtr;
+
+int main(int argc, char* args[]) {
+
+    if(argc < 2) {
+        printf("Error in the usage: soulsand <subcommand>");
+        return -1;
+    }
+
+    if(strcmp(args[1], "create") == 0 || strcmp(args[1], "init") == 0) {
+        printf("[INFO] Creating a new Soulsand server at your location!");
+        filePtr = fopen(".soulsand", "a");
+        fprintf(filePtr, "none\nserver.jar");
+
+        fclose(filePtr);
+
+        return -1;
+    }
+
+    filePtr = fopen(".soulsand", "w");
+
+    if(filePtr == NULL) {
+        printf("Error: This isn't a soulsand environment! Please run soulsand create/init in order to create one!");
+        return -1;
+    }
+
+    // Sub command handling
+    if(strcmp(args[1], "get") == 0) {
+        if(argc < 3) {
+            printf("Error in the usage: soulsand get <version> [mirror]");
+            return -1;
+        }
+
+        char* version = args[2];
+
+        printf("[INFO] Getting version %s", version);
+
+    }
 
 }
